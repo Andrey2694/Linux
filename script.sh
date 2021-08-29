8019792d5d3fa78cbc754fd6c18b6944494cefc5
Andrey2694 <fordcoppola94@gmail.com>
2021-08-29 20:35:33 +0200

    console

---
+++ script.sh
@@ -0,0 +1,7 @@
+cd test
+mkdir -p 11 22 33
+cd 11
+touch test1.txt test2.txt test3.txt test4.json test5.json
+mkdir -p 44 55 66
+ls
+mv test1.txt test2.txt /Users/andrejzmaka/Downloads/testing/qa_course/test
